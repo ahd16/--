3a03fb922cdb86b70c4e1ee7dae34858dcd902e0
ChrisDwitamaPasaribu <ilenium274@gail.com>
2023-09-25 13:44:20 +0700

    LoginController

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+class Auth extends BaseController
+{
+    public function Register()
+    {
+    }
+
+    public function Login()
+    {
+    }
+
+    public function Forgot_Password()
+    {
+    }
+}
