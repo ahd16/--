696d495da0d2aa8fa2d22d70b1f4d1fcadc5a8c9
GALIH <galibsi30@gmail.com>
2023-09-25 14:59:48 +0700

    BookController

---
+++ app/Controllers/BookController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controllers;
+class BookController
+{
+    public function index()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update($id)
+    {
+
+    }
+
+    public function delete($id)
+    {
+
+    }
+}
