789de33b8dfc7fc5608ba5db7b13691c11d850c5
chris <chris911@gmail.com>
2023-10-02 16:17:37 +0700

    database_perpustakaan-19220769

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'perpustakaan-19220769',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
