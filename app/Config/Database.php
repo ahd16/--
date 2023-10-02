ab8ca629a32a51d73716186e16f4dafded2ec670
ahd16 <ahadifbriann@gmail.com>
2023-10-02 15:34:55 +0700

    perpustakaan_19220864

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
+        'hostname'     => '',
+        'username'     => 'localhost',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'perpustakaan_19220864',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
