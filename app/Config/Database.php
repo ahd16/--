a24044905ce1b8615de227ecbc36e8dc1543b996
Edward <edwardrianto24@gmail.com>
2023-10-02 15:28:21 +0700

    Database Perpustakan 19220634

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'Perpustakaan_19220634',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
--- app/Controllers/Penerbit.php
+++ app/Controllers/Penerbit.php
@@ -6,65 +6,13 @@ use App\Controllers\BaseController;
 
 class Penerbit extends BaseController
 {
-    protected $penerbitModel;
-
-    public function __construct()
+    public function Nama_Penerbit()
     {
-        $this->penerbitModel = new PenerbitModel();
     }
 
-    public function index()
+    public function Tahun_Penerbit()
     {
-        $data['penerbit'] = $this->penerbitModel->findAll();
-        return view('penerbit/index', $data);
+
     }
 
-    public function create()
-    {
-        return view('penerbit/create');
-    }
-
-    public function store()
-    {
-        // Lakukan validasi data yang diterima dari form
-
-        // Simpan data ke dalam database menggunakan model
-        $data = [
-            'nama_penerbit' => $this->request->getPost('nama_penerbit')
-            // Tambahkan kolom lain sesuai kebutuhan Anda
-        ];
-        $this->penerbitModel->insert($data);
-
-        // Redirect kembali ke halaman index
-        return redirect()->to('/penerbit');
-    }
-
-    public function edit($id)
-    {
-        $data['penerbit'] = $this->penerbitModel->find($id);
-        return view('penerbit/edit', $data);
-    }
-
-    public function update($id)
-    {
-        // Lakukan validasi data yang diterima dari form
-
-        // Simpan perubahan data ke dalam database menggunakan model
-        $data = [
-            'nama_penerbit' => $this->request->getPost('nama_penerbit')
-            // Tambahkan kolom lain sesuai kebutuhan Anda
-        ];
-        $this->penerbitModel->update($id, $data);
-
-        // Redirect kembali ke halaman index
-        return redirect()->to('/penerbit');
-    }
-
-    public function delete($id)
-    {
-        $this->penerbitModel->delete($id);
-
-        // Redirect kembali ke halaman index
-        return redirect()->to('/penerbit');
-    }
 }
