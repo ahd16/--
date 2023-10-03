69199546789ef4d1d59fc7ac2fc8b6870bc376a9
Galih19220577 <galihbsi30@gmail.com>
2023-10-03 17:35:47 +0700

    Controller-BukuKategori

---
+++ app/Controllers/Controller-buku.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Controller-buku extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
---
+++ app/Controllers/Controller-kategori.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Controller-kategori extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
