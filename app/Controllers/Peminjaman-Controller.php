749cfe43280a38aea693b726bf7e13d920cabc24
ahd16 <ahadifbriann@gmail.com>
2023-10-03 16:16:41 +0700

    Controller-PeminjamanAnggota

---
+++ app/Controllers/Peminjaman-Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Peminjaman-Controller extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
